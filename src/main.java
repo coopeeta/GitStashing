82661025ad77417d7f673f4ac0014193180f68fc
coopeeta <coopeeta@gmail.com>
2022-05-15 12:05:45 -0400

    white audi is ready with basic features

--- src/main.java
+++ src/main.java
@@ -2,7 +2,13 @@ public class main {
 
     public static void main(String[] args) {
 
-        basicFeatures car = new basicFeatures();
+        basicFeatures Audi = new basicFeatures();
 
+        Audi.color="white";
+        Audi.doors=4;
+        Audi.engine="V8";
+        Audi.headlights="blue";
+        Audi.transmission="automatic";
+        
     }
 }
